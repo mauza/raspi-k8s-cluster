2e4a3156512d6d23664765c866ac7e27efaa2a3d
mauza <mauza@mauza.net>
2022-12-28 12:25:19 -0700

    added docker registry changes

--- registry/secret-script.sh
+++ registry/secret-script.sh
@@ -1,3 +1,7 @@
-kubectl create secret docker-registry regcred \
---docker-server=docker.carpie.net --docker-username=registry \
---docker-password=your_reg_password
+#kubectl create secret docker-registry regcred \
+#--docker-server=docker.carpie.net --docker-username=registry \
+#--docker-password=your_reg_password
+
+# You need to create this in each namespace you wish to pull from
+kubectl create secret docker-registry regcred --docker-server=docker.mau.guru --docker-username=registry --docker-password={Actaul Password}
+
